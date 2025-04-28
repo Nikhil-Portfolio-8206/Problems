1ac4a5721ebe34315e328ba548c7287ccf41382b
NIKHIL <nikhil.jmsyadav@gmail.com>
2025-04-28 12:46:33 +0530

    update_p01

--- src/P_01_Java_Basic_Problems/p01_Add_digit_to_last_of_number.java
+++ src/P_01_Java_Basic_Problems/p01_Add_digit_to_last_of_number.java
@@ -1,7 +1,11 @@
 package P_01_Java_Basic_Problems;
-
+import java.util.Scanner;
 public class p01_Add_digit_to_last_of_number {
     public static void main(String[] args) {
-        System.out.println("hello world ");
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+        int add_digit=sc.nextInt();
+        N=N*10+add_digit;
+        System.out.println(N);
     }
 }
